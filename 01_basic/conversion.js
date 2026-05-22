db63fca79dae1fdfea9dc00cf2f4c1ab0f78e66a
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-22 13:03:00 +0000

    conversion of datatypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,24 @@
+let score = "48"
+console.log(typeof score);//score ko kostak me bhi lga sakte h
+
+let valueinnumber = Number(score)//convert karta h number m
+console.log(typeof valueinnumber);//number
+//yadi 33abc  h to nan  aaayega result m
+//yadi null ho to 0 aata h
+//undefined me nan aata h
+//boolean me 0 or 1 ata h
+//true=1,false=0
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);//true
+//empty ka false
+//name ho to true aata h
+let somenumber = 33
+
+let stringnumber = string(stringnumber)
+console.log(stringnumber);//33
+console.log(typeof stringnumber);//string
+
+
+
