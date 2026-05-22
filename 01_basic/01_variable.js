f9d6441848ffb88dc16e92cd5d7b3b1f5f87ce27
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-22 10:53:49 +0000

    variable done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 14434
+let accountEmail = "25bme079@nith.ac.in"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+//accountId = 2//not allowed
+accountEmail = "rahul@gmail.com"
+accountPassword = "4543"
+accountCity = "nawa"
+
+/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
