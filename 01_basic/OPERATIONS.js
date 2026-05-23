3ab2731665331ebdf121cd4a164ff9a43e2d14ef
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-23 11:46:35 +0000

    operations

---
+++ 01_basic/OPERATIONS.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+
+//console.log(negValue);//-3
+
+//console.log(2**2);//4
+
+
+let str1 = "hello"
+let str2 = "rahul"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log(+true);//1 yadi + nhi hota h to true aata h
+
+let num1,num2,num3
+num1 = num2 = num3= 2+2
+
+let Gamecounter = 100
+Gamecounter++;
+console.log(Gamecounter);//101
+
---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,38 @@
+//1 primitive datatypes
+// 7  types : string,number,boolean,null,undefined,symbol,BigInt
+
+
+//reference type (non primitive)
+//array,objects,functions
+
+
+
+const score = 1000
+const scoreValue = 1000.3
+
+const isloggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);//false
+
+const BigNumber = 2453435433455n
+
+
+
+
+
+const heros = ["shaktiman","naagraj","spiderman"]//array
+let myObj = {
+    name: "rahul"
+}
+
+
+function name() {}
+const myfunction = function () {
+    console.log("hello world");
+}
+console.log(typeof BigNumber);//undefined
