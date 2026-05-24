88b02c47bfa5fa1ab7c128b8dcfa7b14d341c2c0
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-24 06:48:51 +0000

    string

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -10,24 +10,5 @@ console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
-
-
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
-
-
-
-const newString = gameName.substring(0,4)
-console.log(newString);
-const anotherString = gameName.slice(-8,4)
-console.log(anotherString);
-const newStringOne = "  rahul  "
-console.log(newStringOne.trim());
-console.log(newStringOne);
-const url = "http://rahul.com/rahul%20jat"
-
-console.log(url.replace('%20','-'))
-
-console.log(url.includes('der'));
-
+                      
 console.log(gameName.split('-'));
