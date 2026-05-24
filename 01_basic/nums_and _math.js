20633e8f37d913b159408f101b9a402ed033534e
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-24 06:47:48 +0000

    nums

---
+++ 01_basic/nums_and _math.js
@@ -0,0 +1,10 @@
+const score = 400
+
+const balance = new number(100)
+//console.log(balance);
+//console.log(balance.tostring().lenght);//100,3
+//console.log(balance.tofixed(2));//100.00
+const othernumber = 34.3232
+//console.log(othernumber.toPrecision(3));//34.3
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en-in'));//10,00,000
