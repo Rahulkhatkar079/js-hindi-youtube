da223bf745c65b280d112378e57e78ba9de9c8a8
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-26 04:30:52 +0000

    maths

--- 01_basic/nums_and _math.js
+++ 01_basic/nums_and _math.js
@@ -8,3 +8,25 @@ const othernumber = 34.3232
 //console.log(othernumber.toPrecision(3));//34.3
 const hundreds = 100000000
 console.log(hundreds.toLocaleString('en-in'));//10,00,000
+
+//maths
+
+console.log(Math);//object
+console.log(Math.abs(-4));//4
+console.log(Math.abs(4));//4
+
+console.log(Math.round(4.4));//4
+console.log(Math.ceil(4.2));//top wali value chose karega mtlb 5
+console.log(Math.floor(4.9));//niche wali value select karega mtlb 4
+console.log(Math.min(4,6,3,7));//3
+//.max bhi hota h
+console.log(Math.random());//0-1 le bich me value aayegi
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min + 1)))//6
+
+
+
+
+
