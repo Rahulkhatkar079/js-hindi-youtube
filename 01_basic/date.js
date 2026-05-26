fef66fcc3202c19029662b84038b56b91634a0dd
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-26 05:16:05 +0000

    date

---
+++ 01_basic/date.js
@@ -0,0 +1,15 @@
+//let mydate = new Date()
+//console.log(mydate.toString());//aaj ka time or date aayega
+//console.log(typeof mydate); //object
+
+let mycreatedDate = new Date(2024,0,24)
+//console.log(mycreatedDate,toString());//jan,24,2024
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(mycreatedDate.getTime());
+console.log(Date.now()/1000);
+
+let newdate = new Date()
+console.log(newdate);
---
+++ array/array.js
@@ -0,0 +1,2 @@
+const myArr = [0,1,2,3,4,rahul,hi]
+console.log(myArr);
