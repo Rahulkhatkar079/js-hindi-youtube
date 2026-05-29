5137e31c150e6053037745bef555038869c34ec6
Rahulkhatkar079 <25bme079@nith.ac.in>
2026-05-29 07:19:42 +0000

    control

---
+++ control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+
+if (true){
+
+}
+if (false) {
+    // nhi execute hoga
+}
+
+const isuserloggedIn = true
+const tempreture = 42
+
+if (tempreture<50) {
+    console.log("less than 50");
+    
+}
+else {
+console.log("tempreture is greater than 50");
+}
+//>,<,<=,>=,==,!=,===
+console.log("execute");
+
+
+const score = 200
+
+if (score>100) {
+    const power = "fly"
+    console.log('User power: ${power}');
+}
+//  console.log('User power: ${power}');
+
